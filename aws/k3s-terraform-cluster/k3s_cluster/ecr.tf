450ba61c7ced27d3c7fd7e14e156a44d8aa757ad
Brandon Samuel <brandon.samuel2010@gmail.com>
2024-04-04 00:54:59 -0400

    fixing issues

--- aws/k3s-terraform-cluster/k3s_cluster/ecr.tf
+++ aws/k3s-terraform-cluster/k3s_cluster/ecr.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  count  = var.global_config.environment == "mgmt" ? 1 : 0
+  #count  = var.global_config.environment == "mgmt" ? 1 : 0
   source = "../modules/ecr"
 
   global_config = var.global_config
