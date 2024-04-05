aee8d530faf2c1a8007aeef9c2b6261063305066
Brandon Samuel <brandon.samuel2010@gmail.com>
2024-04-05 00:18:14 -0400

    stuff

--- aws/k3s-terraform-cluster/get-kubeconfig.sh
+++ aws/k3s-terraform-cluster/get-kubeconfig.sh
@@ -10,7 +10,7 @@ ORG=$(jq '.global_config.organization' -r ./manifest.json)
 
 k3s_kubeconfig=/tmp/k3s_kubeconfig_$ENVIRONMENT
 aws secretsmanager --region $REGION get-secret-value --secret-id k3s-kubeconfig-${NAME}-${ENVIRONMENT}-${ORG}-${ENVIRONMENT}-v2 | jq -r '.SecretString' > $k3s_kubeconfig
-ext_lb_dns=$(aws elbv2 describe-load-balancers --names "k3s-ext-lb-$ENVIRONMENT" | jq -r '.LoadBalancers[].DNSName')
+ext_lb_dns=$(aws elbv2 --region $REGION describe-load-balancers --names "k3s-ext-lb-$ENVIRONMENT" | jq -r '.LoadBalancers[].DNSName')
 k3s_ext_lb_dns=$(echo https://${ext_lb_dns}:6443)
 yq -i -e ".clusters[].cluster.server = \"$k3s_ext_lb_dns\"" /tmp/k3s_kubeconfig_$ENVIRONMENT
 export KUBECONFIG=$k3s_kubeconfig
